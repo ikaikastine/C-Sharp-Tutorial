afab1829931f493ee5d1b5b1d6a6345db7002b29
Kevin Stine <ikaikastine@gmail.com>
2017-05-25 09:38:34 -0700

    Added project

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main()
+		{
+			var name = "Kevin";
+			Console.WriteLine($"Hello {name}!");
+		}
+	}
+}
